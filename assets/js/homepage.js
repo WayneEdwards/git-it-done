c7381804c18e281422f5bf1140269d791417ee91
Wayne Edwards <waedwards42@yahoo.com>
2021-03-28 15:53:03 -0700

    checking for errors

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -58,8 +58,9 @@ var displayRepos = function(repos, searchTerm) {
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
-        repoEl.classList = "list-item flex-row justify-space-between alighn-center";
+        var repoEl = document.createElement("a");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html");
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
@@ -71,6 +72,11 @@ var displayRepos = function(repos, searchTerm) {
         // create a status element
         var statusEl = document.createElement("span");
         statusEl.classList = "flex-row align-center";
+
+        // create a link for each repo
+        var repoEl = document.createElement("a");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
         // check if current repo has issues or not
         if (repos[i].open_issues_count > 0) {
